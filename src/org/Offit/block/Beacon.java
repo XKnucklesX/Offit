774d80c51356ee210a3f238f4ffbf3c4b32f2d60
XKnucklesX <nelsinmartinburnett@gmail>
2013-07-23 01:51:40 +0800

    Update Beacon.java

--- src/org/Offit/block/Beacon.java
+++ src/org/Offit/block/Beacon.java
@@ -1,12 +1,12 @@
 package org.Offit.block;
-<<<<<<< HEAD
 
-import org.Offit.inventory.InventoryHolder;
-=======
 
 import org.Offit.inventory.InventoryHolder;
 
->>>>>>> 270a7627164b3ec128ebc88f047bf50d10ec7aec
+
+import org.Offit.inventory.InventoryHolder;
+
+
 
 public interface Beacon extends BlockState, InventoryHolder {
 }
